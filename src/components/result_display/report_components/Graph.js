9c66f9b042fbae4efd621d11cdbf85ea3b2e8650
Ahmed Elbessfy <Ahmedelbessfy@gmail.com>
2021-09-18 16:20:44 +0200

    set graph img width to fit small screens

--- src/components/result_display/report_components/Graph.js
+++ src/components/result_display/report_components/Graph.js
@@ -22,7 +22,7 @@ const Graph = () => {
       >
         <img
           src={FullChart}
-          style={{ maxWidth: "45rem" }}
+          style={{ width: "100%", maxWidth: "45rem" }}
           alt="بشرة أكثر نضارة بنسبة 97 % و مشاكل بشرة أقل بكثير كل ذلك مع skin plans"
         />
       </div>
