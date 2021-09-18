788627a1f95a6e34da11bf0ab3c7f028e558c5d6
Ahmed Elbessfy <Ahmedelbessfy@gmail.com>
2021-09-18 13:36:28 +0200

    remove padding-left of expecetedchanges details for more clear content

--- src/components/result_display/report_components/ExpectedChanges.js
+++ src/components/result_display/report_components/ExpectedChanges.js
@@ -39,7 +39,7 @@ const ExpectedChanges = () => {
           src={ExpectChangeGraph}
           alt="نضارة 20% و امتلاء بشرة 25% و انتفاخ عين أقل 53% خطوط وجه أقل 75% بقع داكنة أقل 85%"
         />
-        <ul className="h-100 d-flex justify-content-center flex-column px-4">
+        <ul className="h-100 d-flex justify-content-center flex-column pr-4">
           {changes.map((c, i) => {
             return (
               <li key={i}>
