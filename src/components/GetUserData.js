5f7dce74ad7f1f8b295eb5d2b5c8e2cda74631db
Ahmed Elbessfy <Ahmedelbessfy@gmail.com>
2021-09-20 16:32:18 +0200

    remove test backgrounds

--- src/components/GetUserData.js
+++ src/components/GetUserData.js
@@ -113,7 +113,7 @@ const GetUserData = () => {
   return (
     <div
       className="d-flex align-items-center flex-column"
-      style={{ background: "green", overflow: "hidden" }}
+      style={{ height: "85vh" }}
     >
       <div
         style={{
