51805c8627f138e3797cad1c36a35ba336e7a646
Ahmed Elbessfy <Ahmedelbessfy@gmail.com>
2021-09-20 15:38:53 +0200

    add text under submit form btn

--- src/components/FormArrows.js
+++ src/components/FormArrows.js
@@ -1,5 +1,6 @@
 import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
 import { NavLink } from "react-router-dom";
+import { AiFillLock, AiOutlineCheck } from "react-icons/ai";
 
 const FormArrows = ({
   parentProps,
@@ -10,12 +11,27 @@ const FormArrows = ({
   handleSubmit,
 }) => {
   return (
-    <div className="d-flex justify-content-between container flex-row formArrowsContainer">
+    <div
+      className={`d-flex ${
+        !lastStep
+          ? "justify-content-between flex-row"
+          : "justify-content-center"
+      } container formArrowsContainer`}
+      style={
+        lastStep && {
+          width: "auto",
+          maxWidth: "35rem",
+          left: "unset",
+          flexDirection: "column",
+        }
+      }
+    >
       <button
         type="submit"
-        className="btn text-white bg-prime border-0 rounded-pill"
+        className="btn text-white bg-prime border-0 rounded-pill "
         style={{
-          width: `${lastStep && "12rem"}`,
+          width: `${lastStep && "100%"}`,
+          padding: `${lastStep ? "0" : "0.5rem"}`,
         }}
         disabled={condition}
       >
@@ -23,7 +39,7 @@ const FormArrows = ({
         {lastStep ? (
           <NavLink
             to="/results"
-            className="text-white rounded-pill submitFormBtn"
+            className="text-white rounded-pill submitFormBtn "
             data-active={!condition && "active"}
             onClick={() => handleSubmit(values)}
           >
@@ -33,6 +49,22 @@ const FormArrows = ({
           <BsArrowRightShort style={{ fontSize: "2rem" }} />
         )}
       </button>
+      {lastStep && (
+        <div className="mt-2" dir="ltr">
+          <p className="mb-1">
+            {" "}
+            <AiFillLock
+              style={{ fontSize: "1.5rem", marginRight: "0.5rem" }}
+            />{" "}
+            information are 100% secure
+          </p>
+          <p>
+            <AiOutlineCheck style={checkIconStyle} />i understand my information
+            is kept 100% confidential and agree to the terms of service and
+            privacy policy
+          </p>
+        </div>
+      )}
       {!lastStep && !firstStep && (
         <button
           type="button"
@@ -48,4 +80,14 @@ const FormArrows = ({
   );
 };
 
+const checkIconStyle = {
+  width: "2rem",
+  height: "2rem",
+  fontSize: "1.2rem",
+  fontWeight: "900",
+  padding: "0.5rem",
+  marginRight: "0.5rem",
+  border: "1px solid #8f7f7f",
+  color: "#009600",
+};
 export default FormArrows;
