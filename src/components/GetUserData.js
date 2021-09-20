315aa21b7adc72d711b03a1d2a35e2c0d3ee4e6c
Ahmed Elbessfy <Ahmedelbessfy@gmail.com>
2021-09-20 18:25:58 +0200

    modify Info comp style at results page

--- src/components/GetUserData.js
+++ src/components/GetUserData.js
@@ -101,7 +101,7 @@ const GetUserData = () => {
     <SleepingHours next={handleNextStep} prev={handlePrevStep} data={data} />,
     <UsingMakeUp next={handleNextStep} prev={handlePrevStep} data={data} />,
     <DailyPressure next={handleNextStep} prev={handlePrevStep} data={data} />,
-    // <GetUserLocation next={handleNextStep} prev={handlePrevStep} data={data} />,
+    <GetUserLocation next={handleNextStep} prev={handlePrevStep} data={data} />,
     <GetUserPicture next={handleNextStep} prev={handlePrevStep} data={data} />,
     <UserContacts next={handleNextStep} data={data} />,
     // <UserAge next={handleNextStep} prev={handlePrevStep} data={data} />,
